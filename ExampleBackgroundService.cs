7a3e9ad20a3baf047d8199f1f2870a348f93d189
Patrick Westerhoff <PatrickWesterhoff@gmail.com>
2021-10-13 10:43:08 +0200

    Add example background service with db access

---
+++ ExampleBackgroundService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ExampleWorkerConsole
+{
+    public class ExampleBackgroundService : BackgroundService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public ExampleBackgroundService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            // keep running
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                using (var scope = _serviceProvider.CreateScope())
+                {
+                    // retrieve the database context within a new service scope in
+                    // order to allow retrieving the scoped service within the singleton
+                    // background service
+                    var db = scope.ServiceProvider.GetService<DataContext>();
+
+                    // interact with the database
+                    var item = new Item
+                    {
+                        Name = $"Item {DateTime.UtcNow:s}"
+                    };
+                    db.Items.Add(item);
+                    await db.SaveChangesAsync();
+
+                    Console.WriteLine(item.Name);
+                }
+
+                // wait every 15 seconds
+                await Task.Delay(TimeSpan.FromSeconds(15), stoppingToken);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -14,9 +14,28 @@ namespace ExampleWorkerConsole
                 {
                     services.AddDbContext<DataContext>(options =>
                         options.UseSqlite("Data Source=database.db"));
+
+                    // register background service
+                    services.AddHostedService<ExampleBackgroundService>();
                 })
                 .Build();
 
+
+            // if you need to retrieve a service at startup time, then
+            // you can do that through `host.Services` on the built host
+            using (var scope = host.Services.CreateScope())
+            {
+                // of course, if you want to retrieve a database context (or
+                // some other scoped service), then you will need to create a
+                // temporary service scope
+                var db = scope.ServiceProvider.GetService<DataContext>();
+
+                await db.Database.EnsureCreatedAsync();
+            }
+
+
+            // run the host in order to keep the process running while
+            // the hosted (background) services are active
             await host.RunAsync();
         }
     }
