a9723e623496bdd6fb45e2b23ec109d202f1bbed
Patrick Westerhoff <PatrickWesterhoff@gmail.com>
2021-10-13 10:17:03 +0200

    Add console project from template

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExampleWorkerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
